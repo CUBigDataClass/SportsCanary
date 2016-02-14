93578104f1d290388e318c24fb43e8cb270a4547
Frederik Lohner <freddylohner@gmail.com>
2016-02-13 17:07:25 -0700

    Forgot to add files in previous commit

--- Twitter-Utils/SportsData.py
+++ Twitter-Utils/SportsData.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import requests
 import simplejson as json
 
@@ -20,15 +21,23 @@ class SportsData:
         self.STAT_CLIENT_SECRET = os.environ['STAT_CLIENT_SECRET']
         self.STAT_ACCESS_TOKEN = os.environ['STAT_ACCESS_TOKEN']
         self.base_url = 'https://www.stattleship.com/basketball/'
+        self.nba_team_list = ['nba-atl', 'nba-bk', 'nba-bos', 'nba-cha', 'nba-chi',
+                              'nba-cle', 'nba-dal', 'nba-den', 'nba-det', 'nba-gs',
+                              'nba-hou', 'nba-ind', 'nba-lac', 'nba-lal', 'nba-mem',
+                              'nba-mia', 'nba-mil', 'nba-min', 'nba-no', 'nba-ny',
+                              'nba-okc', 'nba-orl', 'nba-phi', 'nba-pho', 'nba-por',
+                              'nba-sa', 'nba-sac', 'nba-tor', 'nba-uta', 'nba-was']
 
-    def get_all_basketball_games(self):
+    def get_games_for_team(self, team_id):
 
         """
+
         Gets all basketball games
+        :param team_id: team_id as defined by stattleship API
         :return: returns content of response
         """
 
-        url = self.base_url + '/nba/games'
+        url = self.base_url + '/nba/games?team_id=' + team_id
         headers = {
             'Authorization': str(self.STAT_ACCESS_TOKEN),
             'Accept': 'application/vnd.stattleship.com; version=1',
@@ -40,10 +49,34 @@ class SportsData:
 
         return content['games']
 
-    def get_games_within_time_range(self):
-        games = self.get_all_basketball_games()
-        print games
+    def get_games_within_time_range_for_team(self, team_id, time_range):
+
+        """
+
+        :param team_id: team_id as defined by stattleship API
+        :param time_range: time range in DAYS given as an INT
+        :return: returns a list with all games within time range
+        """
+        list_next_seven_days = []
+        for x in range(0, time_range):
+            current_date = datetime.datetime.now() + datetime.timedelta(days=x)
+            list_next_seven_days.append(current_date.strftime("%B %d, %Y"))
+
+        games = self.get_games_for_team(team_id=team_id)
+
+        relevant_games_list = []
+
+        for game in games:
+            date_on = game['on'][3:]
+            if date_on in list_next_seven_days:
+                relevant_games_list.append(game)
+
+        return relevant_games_list
+
+
+
+
 
 
 t = SportsData()
-t.get_games_within_time_range()
+t.get_games_within_time_range_for_team('nba-mia', 7)
