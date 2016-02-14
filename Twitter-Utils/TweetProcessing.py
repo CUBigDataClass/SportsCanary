49aabd4c31914e482b394deffb9b2bf39e10ea89
Frederik Lohner <freddylohner@gmail.com>
2016-02-14 01:41:05 -0700

    Added stop word list.

--- Twitter-Utils/TweetProcessing.py
+++ Twitter-Utils/TweetProcessing.py
@@ -10,13 +10,44 @@ After importing text
 
 text1.similar("WORD") returns words with similar meaning to what we are searching for.
 
+After doing some NLP research it seems that there is a thing called a stop word list,
+that essentially means a list of words that add nothing of value to NLP, so we strip these
+from the tweets.
+
+http://www.ranks.nl/stopwords for source
 
 """
 
-
 class TweetProcessor:
     def __init__(self):
-        pass
+        self.stop_word_list = ['a','about','above','after','again','against',
+                  'all','am','an','and','any','are','aren\'t','as',
+                  'at','be','because','been','before','being',
+                  'below','between','both','but','by','can\'t',
+                  'cannot','could','couldn\'t','did','didn\'t',
+                  'do','does','doesn\'t','doing','don\'t','down',
+                  'during','each','few','for','from','further',
+                  'had','hadn\'t','has','hasn\'t','have','haven\'t',
+                  'having','he','he\'d','he\'ll','he\'s','her','here',
+                  'here\'s','hers','herself','him','himself','his',
+                  'how','how\'s','i','i\'d','i\'ll','i\'m','i\'ve',
+                  'if','in','into','is','isn\'t','it','it\'s','its',
+                  'itself','let\'s','me','more','most','mustn\'t',
+                  'my','myself','no','nor','not','of','off','on',
+                  'once','only','or','other','ought','our','ours',
+                  'ourselves','out','over','own','same','shan\'t',
+                  'she','she\'d','she\'ll','she\'s','should',
+                  'shouldn\'t','so','some','such','than','that',
+                  'that\'s','the','their','theirs','them','themselves',
+                  'then','there','there\'s','these','they','they\'d',
+                  'they\'ll','they\'re','they\'ve','this','those',
+                  'through','to','too','under','until','up','very',
+                  'was','wasn\'t','we','we\'d','we\'ll','we\'re',
+                  'we\'ve','were','weren\'t','what','what\'s','when',
+                  'when\'s','where','where\'s','which','while','who',
+                  'who\'s','whom','why','why\'s','with','won\'t','would',
+                  'wouldn\'t','you','you\'d','you\'ll','you\'re','you\'ve',
+                  'your','yours','yourself','yourselves']
 
     @staticmethod
     def standardize_tweet(tweet):
@@ -32,6 +63,7 @@ class TweetProcessor:
 
         :return: returns a single standardized tweet object
         """
+        original_tweet = tweet
         # Convert to lowercase
         tweet = tweet.lower()
 
@@ -64,7 +96,7 @@ class TweetProcessor:
         # Remove's everything that isn't a letter or space
         tweet = re.sub(r'[^a-zA-Z ]','', tweet)
 
-        # Remove repeated char's, currently replaces it with two,
-        # could replace it with one and have it check a dictionary?
+        # Remove repeated char's, currently replaces it with two, could replace it with one and have it check a dictionary?
         tweet = re.sub(r'(.)\1+', r'\1\1', tweet)
+
         return tweet
