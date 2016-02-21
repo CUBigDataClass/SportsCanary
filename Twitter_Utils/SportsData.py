f2c466dd2add51c91353015b5f18fadf8fbbda72
samr13 <shyam08.ceg@gmail.com>
2016-02-21 01:37:07 -0700

    players retrieved on a game day

--- Twitter_Utils/SportsData.py
+++ Twitter_Utils/SportsData.py
@@ -1,6 +1,6 @@
-import os
 import requests
 import simplejson as json
+from Eternal_Utils.CommonUtils import CommonUtils
 
 
 class SportsData:
@@ -14,9 +14,9 @@ class SportsData:
     """
 
     def __init__(self):
-        self.CLIENT_ID = os.environ['STAT_CLIENT_ID']
-        self.STAT_CLIENT_SECRET = os.environ['STAT_CLIENT_SECRET']
-        self.STAT_ACCESS_TOKEN = os.environ['STAT_ACCESS_TOKEN']
+        self.CLIENT_ID = CommonUtils.get_environ_variable('STAT_CLIENT_ID')
+        self.STAT_CLIENT_SECRET = CommonUtils.get_environ_variable('STAT_CLIENT_SECRET')
+        self.STAT_ACCESS_TOKEN = CommonUtils.get_environ_variable('STAT_ACCESS_TOKEN')
         self.base_url = 'https://www.stattleship.com/basketball/'
 
     def get_nba_games_for_today(self):
@@ -55,3 +55,41 @@ class SportsData:
 
         list_of_games = str(list_of_games).replace("'", "")
         return list_of_games
+
+    def get_nba_players_for_today(self):
+        """Gets all players for today"""
+        # TODO - Change back to today rather than date with games
+        url = self.base_url + '/nba/game_logs?on=February-18'
+        #url = self.base_url + '/nba/players?on=February-18'
+        headers = {
+            'Authorization': str(self.STAT_ACCESS_TOKEN),
+            'Accept': 'application/vnd.stattleship.com; version=1',
+            'Content-Type': 'application/json'
+        }
+
+        res = requests.get(url, headers=headers)
+        content = json.loads(res.content)
+        return self.create_players_log_object(content['players'])
+
+    @staticmethod
+    def create_players_log_object(data):
+        """
+        Creates a json object from API call to Stattleship
+        :param data: input of data object from Stattleship
+        :return: returns JSON object with all players name & team_id for each game
+        """
+        games_id=[]
+        list_of_games = json.loads(SportsData().get_nba_games_for_today())
+        for game in list_of_games:
+            games_id.append(game.get('home_team_id'))
+            games_id.append(game.get('away_team_id'))
+
+        list_of_players = []
+        for player in data:
+            if player['team_id'] in games_id:
+                player_name = player['name']
+                player_team_id = player['team_id']
+                list_of_players.append(json.dumps({"name": player_name, "team_id": player_team_id}))
+
+        list_of_players = str(list_of_players).replace("'", "")
+        return list_of_players
