fa5902d4c8bc155ad68661d8fa18f96b582b2768
jcherrera <j.c.h@me.com>
2016-03-08 21:06:00 -0700

    adding nba-logos + fixed stylesheet

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SportsCanary' });
+  res.render('index', { title: 'SportsCanary - Predicting the future with Big Data.' });
 });
 
 module.exports = router;
