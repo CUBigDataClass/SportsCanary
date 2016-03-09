43781a37813ef6e49dfb6be482dec4660ac07bde
Frederik Lohner <freddylohner@gmail.com>
2016-03-09 14:56:56 -0700

    Added base methods for making a POST and DELETE request from python to our API.

---
+++ Eternal_Utils/WebsiteInteraction.py
@@ -0,0 +1,26 @@
+import requests
+import datetime
+import json
+
+
+class WebsiteInteraction:
+    def __init__(self):
+        self.id = 0
+
+    def post_request_to_sports_canary(self, event_name, score_1, score_2, score_applicable):
+        data = {'event_name': event_name, 'score_1': score_1,
+                'score_2': score_2, 'event_date': datetime.datetime.now(),
+                'score_applicable': score_applicable}
+        r = requests.post('http://sportscanary.com/api/results', data=data)
+        print r.status_code
+        json_response = r.text
+        json_response = json.loads(json_response)
+        self.id = json_response['_id']
+
+    def delete_request_to_sports_canary(self):
+        r = requests.delete('http://sportscanary.com/api/results/' + str(self.id) + '/edit')
+        print r.status_code
+#
+# web = WebsiteInteraction()
+# web.post_request_to_sports_canary('Cavaliers vs Rockets', 92, 100, score_applicable=True)
+# web.delete_request_to_sports_canary()
