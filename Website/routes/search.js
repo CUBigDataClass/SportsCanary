ebb7c4ecbd5ccfece1ee65193719d739e6bcc2d9
Frederik Lohner <freddylohner@gmail.com>
2016-03-12 21:22:09 -0700

    /search now returns json with all vs in game name.

--- Website/routes/search.js
+++ Website/routes/search.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
+var elasticsearch = require('elasticsearch');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('search/search', { title: 'SportsCanary - Search' });
+
+    var client = new elasticsearch.Client({
+        host: 'localhost:9200',
+        // Change to info in development
+        log: 'trace'
+    });
+
+    client.search({
+        index: 'sportscanary',
+        body: {
+            query: {
+                match: {
+                    _all: 'vs'
+                }
+            }
+        }
+    }).then(function (resp) {
+        var hits = resp.hits.hits;
+        console.log(hits);
+        res.json(hits)
+    }, function (err) {
+        console.trace(err.message);
+    });
+
+    //res.render('search/search', { title: 'SportsCanary - Search', games: hits });
 });
 
 module.exports = router;
