6a6269b8d591a2542774ed585f9b74029cf3f2ee
Frederik Lohner <freddylohner@gmail.com>
2016-03-12 21:46:51 -0700

    Now pulls search term from search bar and queries mongodb.

--- Website/routes/search.js
+++ Website/routes/search.js
@@ -1,33 +1,40 @@
 var express = require('express');
+var url = require('url');
 var router = express.Router();
 var elasticsearch = require('elasticsearch');
-/* GET home page. */
 router.get('/', function(req, res, next) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var cat = query.category;
 
-    var client = new elasticsearch.Client({
-        host: 'localhost:9200',
-        // Change to info in development
-        log: 'trace'
-    });
 
-    client.search({
-        index: 'sportscanary',
-        body: {
-            query: {
-                match: {
-                    _all: 'vs'
+    if(cat == "" || cat == null) {
+        res.render('search/search', { title: 'SportsCanary - Search'});
+    } else {
+        var client = new elasticsearch.Client({
+            host: 'localhost:9200',
+            // Change to trace in development
+            log: 'info'
+        });
+
+        client.search({
+            index: 'sportscanary',
+            body: {
+                query: {
+                    match: {
+                        _all: cat
+                    }
                 }
             }
-        }
-    }).then(function (resp) {
-        var hits = resp.hits.hits;
-        console.log(hits);
-        res.json(hits)
-    }, function (err) {
-        console.trace(err.message);
-    });
-
-    //res.render('search/search', { title: 'SportsCanary - Search', games: hits });
+        }).then(function (resp) {
+            var hits = resp.hits.hits;
+            console.log(hits);
+            console.log(cat);
+            res.json(hits)
+        }, function (err) {
+            console.trace(err.message);
+        });
+    }
 });
 
 module.exports = router;
