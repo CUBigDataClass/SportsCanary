03d2431950891def90563da7666edff34f72fc4f
Frederik Lohner <freddylohner@gmail.com>
2016-03-15 11:59:33 -0600

    Added skeleton tests for TwitterClient and MarchMadness

---
+++ Twitter_Utils/tests/test_TwitterClient.py
@@ -0,0 +1,13 @@
+import unittest
+from Twitter_Utils.TwitterClient import TwitterClient
+
+
+class TestTwitterClient(unittest.TestCase):
+    def test___init__(self):
+        assert True
+
+    def test_tweet(self):
+        assert True
+
+if __name__ == '__main__':  # pragma: no cover
+    unittest.main()
