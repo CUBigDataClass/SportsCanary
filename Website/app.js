d1e8a8cfb737dd1fd92bca21e558bd94e603ad1d
jcherrera <j.c.h@me.com>
2016-03-18 20:13:05 -0600

    Sports Layout :skeleton:

--- Website/app.js
+++ Website/app.js
@@ -40,7 +40,7 @@ app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('404: Not Found');
   err.status = 404;
   next(err);
 });
