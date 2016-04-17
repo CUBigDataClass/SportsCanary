8d36edaf1d4e86a73e4d80656419f92b57bdf0e0
Frederik Lohner <freddylohner@gmail.com>
2016-04-16 23:02:46 -0600

    Fixed index bug.  Now saves and loads json blob properly.

--- Gnip_Client/SearchClient.py
+++ Gnip_Client/SearchClient.py
@@ -9,24 +9,41 @@ class GnipSearchClient:
         self.username = os.environ['GNIP_USERNAME']
         self.password = os.environ['GNIP_PASSWORD']
         self.url = os.environ['GNIP_URL']
+        self.counter = 0
 
     # Date is done by UTC, which is a five hour difference to EST.
     # Kobe's last game was 10:30 EST, so 2:30 AM UTC on the 14
-    def initial_search(self, query, max_results, from_date, to_date):
+    def initial_search(self, query, max_results, from_date, to_date, number_of_pages):
         from_date = '201604140230'
         to_date = '201604140530'
-        max_results = 10
-        next = ''
-        for i in range(2):
-            query = '?query=' + str(query) + '&maxResults=' + str(max_results)
-            date = '&fromDate=' + str(from_date) + '&toDate=' + str(to_date)
-            if next == '':
+        max_results = 500
+        next_token = ''
+        query = '?query=' + str(query) + '&maxResults=' + str(max_results)
+        date = '&fromDate=' + str(from_date) + '&toDate=' + str(to_date)
+        for i in range(number_of_pages):
+            if next_token == '':
                 url = self.url + query + date
             else:
-                url = self.url + query + date + next
+                url = self.url + query + date + '&next=' + next_token
 
             r = requests.get(url, auth=HTTPBasicAuth(self.username, self.password))
             content = json.loads(r.content)
-            next = content['next']
-            print(content['next'])
 
+            self.save_json_blog_to_disk(content, self.counter)
+            self.counter += 1
+            next_token = content['next']
+
+    def save_json_blog_to_disk(self, json_blob, counter):
+        file_path = self.get_file_path(counter)
+        with open(file_path, 'w') as outfile:
+            json.dump(json_blob, outfile)
+
+    @staticmethod
+    def get_file_path(counter):
+        wd = os.getcwd()
+        return wd + '/Gnip_Client/Gnip_Search_' + str(counter) + '.txt'
+
+    def load_json_blob(self, counter):
+        file_path = self.get_file_path(counter)
+        with open(file_path) as data_file:
+            return json.load(data_file)
