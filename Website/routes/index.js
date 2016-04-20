705e7def6f033e73a8a677f2dac119a0059786fb
Frederik Lohner <freddylohner@gmail.com>
2016-04-20 13:50:58 -0600

    Added cumulative results mode, added method to ScoreUpdater to automatically update those values on Mongo.

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var cumulative_result = mongoose.model('CumulativeResult');
 var result = mongoose.model('Result');
 
 /* GET home page. */
@@ -17,16 +18,26 @@ router.get('/', function(req, res, next) {
             if (err) {
               return console.error(err);
             } else {
-              res.format({
-                html: function () {
-                  res.render('index', {
-                    title: 'SportsCanary - Predicting the future of Sports with Big Data.',
-                    "mlb_results": mlb_results,
-                    "nba_results": nba_results,
-                    "nhl_results": nhl_results
+              cumulative_result.find({}, function (err, cumulative_nhl_result) {
+                if (err) {
+                  return console.error(err);
+                } else {
+                  console.log('kekeke');
+                  console.log(cumulative_nhl_result);
+                  console.log(mlb_results);
+                  res.format({
+                    html: function () {
+                      res.render('index', {
+                        title: 'SportsCanary - Predicting the future of Sports with Big Data.',
+                        "mlb_results": mlb_results,
+                        "nba_results": nba_results,
+                        "nhl_results": nhl_results,
+                        "nhl_cumulative_result": cumulative_nhl_result
+                      });
+                    }
                   });
                 }
-              });
+              }).limit(1).sort({'event_date': -1});
             }
           }).limit(1).sort({'event_date': -1});
         }
