41b6a7b75e0f541fcdb5ed39a9d4a057e6208258
Frederik Lohner <freddylohner@gmail.com>
2016-04-19 19:32:41 -0600

    Added endpoints for nhl nba and mlb.

--- Website/routes/sports.js
+++ Website/routes/sports.js
@@ -20,4 +20,64 @@ router.route('/')
         }
     });
 });
+router.route('/nba').get(function(req, res, next) {
+    mongoose.model('Result').find({'sport_type': 'nba'}, function (err, results_nba) {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.format({
+                html: function(){
+                    results_nba = results_nba.reverse();
+                    res.render('sports/results-nba', {
+                        title: 'Results',
+                        "results_nba" : results_nba
+                    });
+                },
+                json: function(){
+                    res.json(results_nba);
+                }
+            });
+        }
+    });
+});
+router.route('/nhl').get(function(req, res, next) {
+    mongoose.model('Result').find({'sport_type': 'nhl'}, function (err, results_nhl) {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.format({
+                html: function(){
+                    results_nhl = results_nhl.reverse();
+                    res.render('sports/results-nhl', {
+                        title: 'Results',
+                        "results_nhl" : results_nhl
+                    });
+                },
+                json: function(){
+                    res.json(results_nhl);
+                }
+            });
+        }
+    });
+});
+router.route('/mlb').get(function(req, res, next) {
+    mongoose.model('Result').find({'sport_type': 'mlb'}, function (err, results_mlb) {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.format({
+                html: function(){
+                    results_mlb = results_mlb.reverse();
+                    res.render('sports/results-mlb', {
+                        title: 'Results',
+                        "results_mlb" : results_mlb
+                    });
+                },
+                json: function(){
+                    res.json(results_mlb);
+                }
+            });
+        }
+    });
+});
 module.exports = router;
