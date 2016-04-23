cd5a92cff739b056040d54cf06a93991900c91f9
Frederik Lohner <freddylohner@gmail.com>
2016-04-23 13:02:57 -0600

    Search now searches for team names and match names.

--- Website/routes/search.js
+++ Website/routes/search.js
@@ -1,38 +1,61 @@
 var express = require('express');
 var url = require('url');
+var mongoose = require('mongoose');
 var router = express.Router();
 var elasticsearch = require('elasticsearch');
+var result = mongoose.model('Result');
+
 router.get('/', function(req, res, next) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
-    var cat = query.category;
+    var category = query.category;
 
-
-    if(cat === "" || cat === null) {
+    if(category === "" || category === null) {
         res.render('search/search', { title: 'SportsCanary - Search'});
     } else {
-        var client = new elasticsearch.Client({
-            host: 'localhost:9200',
-            // Change to trace in development
-            log: 'info'
-        });
-
-        client.search({
-            index: 'sportscanary',
-            body: {
-                query: {
-                    match: {
-                        _all: cat
-                    }
+        //var client = new elasticsearch.Client({
+        //    host: 'localhost:9200',
+        //    // Change to trace in development
+        //    log: 'info'
+        //});
+        result.find({ $or: [{'team_1_name': category}, {'team_2_name': category}, {'event_name': category}]}, function (err, results) {
+            if (err) {
+                return console.error(err);
+            } else {
+                if (results.length == 0) {
+                    // TODO - Handle no results, we could 404.
+                    res.status(404)
                 }
+                res.format({
+                    html: function(){
+                        results = results.reverse();
+                        res.render('sports/results-index', {
+                            title: 'SportsCanary - Predictions for Basketball, Hockey and Basketball',
+                            "results" : results
+                        });
+                    },
+                    json: function(){
+                        res.json(results);
+                    }
+                });
             }
-        }).then(function (resp) {
-            var hits = resp.hits.hits;
-            res.json(hits);
-        }, function (err) {
-            console.trace(err.message);
-            res.render('search/search', { title: 'SportsCanary - Search'});
         });
+        //client.search({
+        //    index: 'sportscanary',
+        //    body: {
+        //        query: {
+        //            match: {
+        //                _all: cat
+        //            }
+        //        }
+        //    }
+        //}).then(function (resp) {
+        //    var hits = resp.hits.hits;
+        //    res.json(hits);
+        //}, function (err) {
+        //    console.trace(err.message);
+        //    res.render('search/search', { title: 'SportsCanary - Search'});
+        //});
     }
 });
 
