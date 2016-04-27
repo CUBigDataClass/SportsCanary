ff2543a5f3ced1cdfd4b14d20254adb87f2fc20f
jcherrera <j.c.h@me.com>
2016-04-27 15:47:24 -0600

    new odometers for homepage

--- Website/public/js/switch.js
+++ Website/public/js/switch.js
@@ -1,3 +1,12 @@
 setTimeout(function(){
     odometer.innerHTML = 2527503;
 }, 1000);
+setTimeout(function(){
+    odometer2.innerHTML = 49.3;
+}, 1000);
+setTimeout(function(){
+    odometer3.innerHTML = 69.8;
+}, 1000);
+setTimeout(function(){
+    odometer4.innerHTML = 57.6;
+}, 1000);
